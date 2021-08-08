9e312b124a004e02e159045854707fee74de7dbe
YoungBear <youngbear@aliyun.com>
2021-08-08 18:06:41 +0800

    [add] add AESUtils

---
+++ src/main/java/com/ysx/utils/crypto/aes/AESUtils.java
@@ -0,0 +1,98 @@
+package com.ysx.utils.crypto.aes;
+
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.GCMParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.*;
+import java.security.spec.AlgorithmParameterSpec;
+
+/**
+ * @author youngbear
+ * @email youngbear@aliyun.com
+ * @date 2021/8/8 9:34
+ * @blog https://blog.csdn.net/next_second
+ * @github https://github.com/YoungBear
+ * AES 加解密
+ * 实践中，可以封装加解密方法，封装异常抛出统一异常
+ * 密文格式一般为：加密算法id+密钥id+iv值+密文数据
+ * 结合密钥管理系统，缓存，实现加解密
+ * 本实例使用 AES/GCM/NoPadding 为例
+ */
+public class AESUtils {
+
+    /**
+     * AES 加密算法
+     * AES/GCM/NoPadding"
+     * 加密后的密文数据，长度是密钥长度的正数倍
+     */
+    private static final String CIPHER_ALGORITHM = "AES/GCM/NoPadding";
+
+    /**
+     * GCM 消息认证码长度(bit) 128bit
+     */
+    private static final int GCM_AUTH_TAG_LENGTH = 8 * 16;
+
+    /**
+     * AES 算法名称
+     */
+    private static final String AES_ALGORITHM_NAME = "AES";
+
+    static {
+        Security.addProvider(new BouncyCastleProvider());
+    }
+
+    /**
+     * 加密
+     *
+     * @param secretKey 密钥
+     * @param iv        iv值
+     * @param plainData 待加密的明文数据
+     * @return 密文
+     * @throws NoSuchPaddingException             异常
+     * @throws NoSuchAlgorithmException           异常
+     * @throws NoSuchProviderException            异常
+     * @throws InvalidAlgorithmParameterException 异常
+     * @throws InvalidKeyException                异常
+     * @throws IllegalBlockSizeException          异常
+     * @throws BadPaddingException                异常
+     */
+    public static byte[] encrypt(byte[] secretKey, byte[] iv, byte[] plainData)
+            throws NoSuchPaddingException, NoSuchAlgorithmException, NoSuchProviderException,
+            InvalidAlgorithmParameterException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+        Cipher cipher = Cipher.getInstance(CIPHER_ALGORITHM, BouncyCastleProvider.PROVIDER_NAME);
+        AlgorithmParameterSpec parameterSpec = new GCMParameterSpec(GCM_AUTH_TAG_LENGTH, iv);
+        Key secretKeySpec = new SecretKeySpec(secretKey, AES_ALGORITHM_NAME);
+        cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec, parameterSpec);
+        return cipher.doFinal(plainData);
+    }
+
+    /**
+     * 解密
+     *
+     * @param secretKey  密钥
+     * @param iv         iv值
+     * @param cipherData 待解密的密文数据
+     * @return 明文
+     * @throws NoSuchPaddingException             异常
+     * @throws NoSuchAlgorithmException           异常
+     * @throws NoSuchProviderException            异常
+     * @throws InvalidAlgorithmParameterException 异常
+     * @throws InvalidKeyException                异常
+     * @throws IllegalBlockSizeException          异常
+     * @throws BadPaddingException                异常
+     */
+    public static byte[] decrypt(byte[] secretKey, byte[] iv, byte[] cipherData)
+            throws NoSuchPaddingException, NoSuchAlgorithmException, NoSuchProviderException,
+            InvalidAlgorithmParameterException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+        Cipher cipher = Cipher.getInstance(CIPHER_ALGORITHM, BouncyCastleProvider.PROVIDER_NAME);
+        AlgorithmParameterSpec parameterSpec = new GCMParameterSpec(GCM_AUTH_TAG_LENGTH, iv);
+        Key secretKeySpec = new SecretKeySpec(secretKey, AES_ALGORITHM_NAME);
+        cipher.init(Cipher.DECRYPT_MODE, secretKeySpec, parameterSpec);
+        return cipher.doFinal(cipherData);
+    }
+}
---
+++ src/test/java/com/ysx/utils/crypto/aes/AESUtilsTest.java
@@ -0,0 +1,106 @@
+package com.ysx.utils.crypto.aes;
+
+import org.bouncycastle.util.encoders.Hex;
+import org.junit.Assert;
+import org.junit.Test;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.security.*;
+
+/**
+ * @author youngbear
+ * @email youngbear@aliyun.com
+ * @date 2021/8/8 9:45
+ * @blog https://blog.csdn.net/next_second
+ * @github https://github.com/YoungBear
+ * @description
+ */
+public class AESUtilsTest {
+
+    /**
+     * 测试用的密钥，实践中需要随机生成，并使用密钥系统管理
+     * 128bit
+     */
+    private static final String SECRET_KEY_HEX_STRING = "4bf05f9e002f650920cdc6a4e274c41a749f1995fa55958bdcb4cc3d1f029b70";
+
+    /**
+     * 测试用的IV值，实践中，每次加密随机生成
+     * 128bit
+     */
+    private static final String IV_HEX_STRING = "775048c30bc37d567bef7681512dfb4b411fc030048912f208e83b598e690ad5";
+
+
+    /**
+     * 测试用的明文数据
+     * 384bit
+     */
+    private static final String PLAIN_DATA_HEX_STRING = "d80517332cd3f7807ddcbf79c999f93483715716ec9b97094d65872650de633bb975aa3ac5587972eb2609e8935b7e09";
+
+    /**
+     * 测试用的密文数据（使用AES 256bit的密钥加密后，密文长度为256bit的整数倍）
+     * 512bit
+     */
+    private static final String CIPHER_DATA_HEX_STRING = "3559d4020986bfd846a0cf24df9dea53fb0a90ec199ae6d488146e081e609e1a1957a9ad5650269f305a57c6f34477a8484d7a33e5348c5cf85b09d32eb82a59";
+
+
+    @Test
+    public void encryptSuccessTest()
+            throws InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException,
+            NoSuchAlgorithmException, BadPaddingException, NoSuchProviderException, InvalidKeyException {
+        byte[] secretKey = Hex.decode(SECRET_KEY_HEX_STRING);
+        byte[] iv = Hex.decode(IV_HEX_STRING);
+        byte[] plainData = Hex.decode(PLAIN_DATA_HEX_STRING);
+        byte[] cipherData = AESUtils.encrypt(secretKey, iv, plainData);
+        Assert.assertArrayEquals(Hex.decode(CIPHER_DATA_HEX_STRING), cipherData);
+        Assert.assertEquals(CIPHER_DATA_HEX_STRING, Hex.toHexString(cipherData));
+    }
+
+    @Test
+    public void encryptFailedTest()
+            throws InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException,
+            NoSuchAlgorithmException, BadPaddingException, NoSuchProviderException, InvalidKeyException {
+        byte[] secretKey = Hex.decode(SECRET_KEY_HEX_STRING);
+        byte[] iv = Hex.decode(IV_HEX_STRING);
+        // 不同的明文，密文不同
+        byte[] plainData = Hex.decode(PLAIN_DATA_HEX_STRING + "fa");
+        byte[] cipherData = AESUtils.encrypt(secretKey, iv, plainData);
+        Assert.assertNotEquals(CIPHER_DATA_HEX_STRING, Hex.toHexString(cipherData));
+    }
+
+    @Test
+    public void decryptSuccessTest()
+            throws InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException,
+            NoSuchAlgorithmException, BadPaddingException, NoSuchProviderException, InvalidKeyException {
+        byte[] secretKey = Hex.decode(SECRET_KEY_HEX_STRING);
+        byte[] iv = Hex.decode(IV_HEX_STRING);
+        byte[] cipherData = Hex.decode(CIPHER_DATA_HEX_STRING);
+        byte[] plainData = AESUtils.decrypt(secretKey, iv, cipherData);
+        Assert.assertArrayEquals(Hex.decode(PLAIN_DATA_HEX_STRING), plainData);
+        Assert.assertEquals(PLAIN_DATA_HEX_STRING, Hex.toHexString(plainData));
+    }
+
+    @Test
+    public void decryptFailedTest()
+            throws InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException,
+            NoSuchAlgorithmException, BadPaddingException, NoSuchProviderException, InvalidKeyException {
+        byte[] secretKey = Hex.decode(SECRET_KEY_HEX_STRING);
+        byte[] iv = Hex.decode(IV_HEX_STRING);
+        byte[] cipherData = Hex.decode(CIPHER_DATA_HEX_STRING);
+        byte[] plainData = AESUtils.decrypt(secretKey, iv, cipherData);
+        // excepted 为错误的明文
+        Assert.assertNotEquals(PLAIN_DATA_HEX_STRING + "fa", Hex.toHexString(plainData));
+    }
+
+
+    /**
+     * 生成随机数
+     */
+    private void random() {
+        SecureRandom random = new SecureRandom();
+        byte[] data = new byte[48];
+        random.nextBytes(data);
+        System.out.println(Hex.toHexString(data));
+    }
+}
